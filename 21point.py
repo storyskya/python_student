9a9930086a4ed23a9503f1a26cf5c851a0426e93
storyskya <storysky@aliyun.com>
2019-03-30 17:46:13 +0800

    21

--- 21point.py
+++ 21point.py
@@ -2,32 +2,41 @@
 # -*- coding=utf-8 -*-
 # filename 21point.py
 # author MaxD
+import sys
 import string
 import random
 readme = '欢迎来到21点游戏'
 print(readme.center(30,'#'))
-user_num = input('请输入玩家人数：')
+user_num = input('请输入玩家人数,限制为4人：')
 name_list = []
-for i in range(1,int(user_num)+1):
-    user_name = input(f'输入玩家{i}名字: ')
-    name_list.append(user_name)
-print(" ".join(name_list))
+if int(user_num) <= 4:
+    for i in range(1,int(user_num)+1):
+        user_name = input(f'输入玩家{i}名字: ')
+        name_list.append(user_name)
+    print(f'玩家列表为：{" ".join(name_list)}')
+else:
+    print('请输入正确的数值')
+    sys.exit()
 
+# 第一轮点数
 one_list=[]
 for _ in range(int(user_num)):
-    p1 = random.randint(1,10)
+    p1 = random.randint(5,10)
     one_list.append(p1)
 for one in one_list:
     print(one,end = " ")
 print()
+
+# 第二轮点数
 two_list=[]
 for _ in range(int(user_num)):
-    p1 = random.randint(1,10)
+    p1 = random.randint(5,10)
     two_list.append(p1)
 for two in two_list:
     print(two,end = " ")
 print()
 
+# 前两轮点数和
 sum_list=[]
 for sum in range(0,len(one_list)):
     sum_1 = one_list[sum] + two_list[sum]
@@ -36,11 +45,69 @@ for sum3 in sum_list:
     print(sum3,end = " ")
 print()
 
+# 生成一个词典
 user_dict = {}
-for yuansu in range(0,len(sum_list)):
+for yuansu in range(0,len(name_list)):
     user_dict[name_list[yuansu]] = sum_list[yuansu]
+print(user_dict)
 
-# for a in range(0,len(sum_list)):
-#     user_con = input(f'{name_list[a]} 是否继续？(y/n)')
-#     if user1_con == 'n':
-#         new_point = del 
+# 询问玩家是否继续
+# three_list = []
+name1_list = []
+user1_dict = user_dict.copy()
+end_dict = {}
+for a in range(0,len(sum_list)):
+    user_con = input(f'{name_list[a]} 是否继续？(y/n)')
+    if user_con == 'y':
+        # three_list.append(sum_list[a])
+        name1_list.append(name_list[a])
+    elif user_con == 'n':
+        end_dict[name_list[a]] = sum_list[a]
+        del user1_dict[name_list[a]]
+print(f'所剩玩家{name1_list}')
+print(30*'-')
+print(f'已停手玩家{end_dict}')
+print(30*'-')
+print(f'当前玩家得分{user_dict}')
+print(30*'-')
+
+four_list=[]
+for _ in range(0,len(name1_list)):
+    p1 = random.randint(5,10)
+    four_list.append(p1)
+for four in four_list:
+    print(four,end = " ")
+print()
+
+# 第四轮点数和
+sum4_list=[]
+for sum4 in range(0,len(four_list)):
+    sum_3 = sum_list[sum4] + four_list[sum4]
+    sum4_list.append(sum_3)
+for sum5 in sum4_list:
+    print(sum5,end = " ")
+print()
+print(end_dict)
+name2_list = []
+five_list = []
+for e in range(0,len(name1_list)):
+    user_con = input(f'{name1_list[e]} 是否继续？(y/n)')
+    if user_con == 'y':
+        five_list.append(sum4_list[e])
+        name2_list.append(name1_list[e])
+    elif user_con == 'n':
+        end_dict[name1_list[e]] = sum4_list[e]
+        del user1_dict[name1_list[e]]
+print(end_dict)
+
+sort_list = []
+if user1_dict == {}:
+    for u in name_list:
+        if end_dict[u] <= 21:
+            sort_list.append(end_dict[u])
+    sort_list.sort(reverse=True)
+for name,point in end_dict.items():
+    if point == sort_list[0]:
+        print(f'{name} Win point is {point}')
+            
+
