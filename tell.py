890692adbea42d82a9351b2cc1ad12a08231a7f2
storyskya <storysky@aliyun.com>
2019-04-13 14:33:23 +0800

    new

--- tell.py
+++ tell.py
@@ -1,18 +1,7 @@
-import random
-# print(random.randint(21))
-# list1 = []
-# list1.append
-# name_list = []
-# name_num = 1
-# list1 = [1,3,4,5]
-# print(list1)
-# print("".join(list1))
-one_list = [1,2,4,5,6,7]
-# x = 0
-# for _ in range(3):
-#     p1 = random.randint(1,10)
-#     one_list.append(p1)
-#     print(one_list[x])
-#     x += 1
-for i in one_list:
-    print (i)
+dict1 = {'a':1,'b':2,'c':3}
+print(id(dict1))
+dict2 = dict1.copy()
+print(id(dict2))
+dict2['a'] = 3
+print(dict1)
+print(dict2)
