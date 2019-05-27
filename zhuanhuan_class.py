470e86dd85df35308c4c2e95112f463a2b42dcef
storyskya <storysky@aliyun.com>
2019-05-27 18:10:58 +0800

    class

--- zhuanhuan_class.py
+++ zhuanhuan_class.py
@@ -7,45 +7,66 @@ import string
 import re
 import os
 
-def wendu():
-    temp = input('please input 温度(例如 1c 或者 1f):')
-    if temp.endswith('c'):
-        temp = float(temp.strip(string.ascii_letters))
-        tf = (9/5) * temp + 32
-        print(f'tf = (9/5)*{temp}+32 = {tf}F')
-    elif temp.endswith('f'):
+
+class wendu:
+
+    def Hua_She():
         temp = float(temp.strip(string.ascii_letters))
         tf = (5/9) * temp - 32
-        print(f'tf = (5/9)*{temp}-32 = {tf}T')
-    else:
-        print('bye')
-    return()
+        return tf
 
-def huobi():
-    temp = input('please input 货币(例如 1rmb 或者 1usd):')
-    if temp.endswith('rmb'):
+    def She_Hua():
         temp = float(temp.strip(string.ascii_letters))
-        usd = temp / 6.78
-        print(f'{temp}rmb = {temp}/6.78 = {usd}usd')
-    elif temp.endswith('usd'):
-        temp = float(temp.strip(string.ascii_letters))
-        rmb=  temp * 6.78
-        print(f'{temp}usd = {temp} * 6.78 = {rmb}rmb')
-    else:
-        print('bye')
+        tf = (9/5) * temp + 32
+        return tf
 
-def changdu():
-    temp = input('please input 长度(例如 1M 或者 1ft):')
-    if temp.endswith('M'):
-        temp = float(temp.strip(string.ascii_letters))
-        m = temp * 3.28
-        print(f'{temp}M = {temp} * 3.28 = {m}ft')
-    elif temp.endswith('ft'):
-        temp = float(temp.strip(string.ascii_letters))
-        ft=  temp / 3.28
-        print(f'{temp}ft = {temp} / 3.28 = {ft}M')
-    else:
-        print('bye')
+    def wendu():
+        temp = input('please input 温度(例如 1c 或者 1f):')
+        if temp.endswith('c'):
+            temp = float(temp.strip(string.ascii_letters))
+            tf = (9/5) * temp + 32
+            print(f'tf = (9/5)*{temp}+32 = {tf}F')
+        elif temp.endswith('f'):
+            temp = float(temp.strip(string.ascii_letters))
+            tf = (5/9) * temp - 32
+            print(f'tf = (5/9)*{temp}-32 = {tf}T')
+        else:
+            print('bye')
+        return()
+
+class huobi:
+
+    def huobi():
+        temp = input('please input 货币(例如 1rmb 或者 1usd):')
+        if temp.endswith('rmb'):
+            temp = float(temp.strip(string.ascii_letters))
+            usd = temp / 6.78
+            print(f'{temp}rmb = {temp}/6.78 = {usd}usd')
+        elif temp.endswith('usd'):
+            temp = float(temp.strip(string.ascii_letters))
+            rmb=  temp * 6.78
+            print(f'{temp}usd = {temp} * 6.78 = {rmb}rmb')
+        else:
+            print('bye')
+
+
+class changdu:
+    def changdu():
+        temp = input('please input 长度(例如 1M 或者 1ft):')
+        if temp.endswith('M'):
+            temp = float(temp.strip(string.ascii_letters))
+            m = temp * 3.28
+            print(f'{temp}M = {temp} * 3.28 = {m}ft')
+        elif temp.endswith('ft'):
+            temp = float(temp.strip(string.ascii_letters))
+            ft=  temp / 3.28
+            print(f'{temp}ft = {temp} / 3.28 = {ft}M')
+        else:
+            print('bye')
+
+
+class IO:
+    pass
 
 while True:
     print('Wellcome'.center(30,'*'))
